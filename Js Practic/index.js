2361340ef71f471aba48ff8c08e99f8726c9cfc7
Bhagvan Pavan <bhagavanpavan999@gmail.com>
2025-03-09 11:37:37 +0530

    Improve new features

--- Js Practic/index.js
+++ Js Practic/index.js
@@ -1,16 +1,72 @@
 let todoItemsContainer = document.getElementById("todoItemsContainer");
 
-let todoElement = document.createElement("li");
-todoElement.classList.add("todo-item-container","d-flex","flex-row");
-todoItemsContainer.appendChild(todoElement);
 
-let inputElement = document.createElement("input");
-inputElement.type = "checkbox";
-inputElement.id =  "checkboxInput";
-inputElement.classList.add("checkbox-input");
-todoElement.appendChild(inputElement);
+function createAndAppendTodo(todo) {
+    let todoElement = document.createElement("li");
+    todoElement.classList.add("todo-item-container", "d-flex", "flex-row");
+    todoItemsContainer.appendChild(todoElement);
 
-let labelContainer = document.createElement("div");
-labelContainer.classList.add("lable-container","d-flex","flex-row");
-todoElement.appendChild(labelContainer);
+    let inputElement = document.createElement("input");
+    inputElement.type = "checkbox";
+    inputElement.id = "checkboxInput";
+    inputElement.classList.add("checkbox-input");
+    todoElement.appendChild(inputElement);
 
+    let labelContainer = document.createElement("div");
+    labelContainer.classList.add("label-container", "d-flex", "flex-row");
+    todoElement.appendChild(labelContainer);
+
+
+    let labelElement = document.createElement("label");
+    labelElement.setAttribute("for", "checkboxInput");
+    labelElement.classList.add("checkbox-label");
+    labelElement.textContent = todo.text;
+    labelContainer.appendChild(labelElement);
+
+    let deleteIconContainer = document.createElement("div");
+    deleteIconContainer.classList.add("delete-icon-container");
+    labelContainer.appendChild(deleteIconContainer);
+
+
+    let deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("far", "fa-trash-alt", "delete-icon");
+    deleteIconContainer.appendChild(deleteIcon);
+}
+
+let todoList = [
+{
+    text: "Learn HTML"
+},
+{
+    text: "Learn CSS"
+},
+{
+    text: "Learn JavaScript"
+},
+{
+    text: "Learn React"
+},
+{
+    text: "Learn NodeJS"
+},
+{
+    text: "Learn Python"
+},
+{
+    text: "Learn React"
+},
+{
+    text: "Learn NodeJS"
+},
+{
+    text: "Learn Python"
+},
+];
+
+/*createAndAppendTodo(todoList[0]);
+createAndAppendTodo(todoList[1]);
+createAndAppendTodo(todoList[2]); */
+
+ for (let eachTodo of todoList){
+    createAndAppendTodo(eachTodo);
+ }
