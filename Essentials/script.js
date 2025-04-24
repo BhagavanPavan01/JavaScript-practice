15c7cafc79799da0ff155930101cf2a8588a0ed5
Bhagvan Pavan <bhagavanpavan999@gmail.com>
2025-04-24 23:20:18 +0530

    More Practice

--- Essentials/script.js
+++ Essentials/script.js
@@ -79,10 +79,259 @@
 // console.log(result); 
 
 
-let person = {
+// let person = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// };
+// console.log(`my details are ${JSON.stringify(person)}`); // Output: my details are {"name":"John","age":30,"city":"New York"}  
+
+
+
+
+// let a = 10;
+// let b = 20;
+
+// (a<b) ? console.log("a is less") : console.log("b is less");
+
+
+// let speed = 70;
+// let message = speed >= 100 ? "Too fast" : "Ok";
+// console.log(message); 
+
+
+
+// let day = 7;
+ 
+// switch(day){
+//     case 0:
+//         console.log("Sunday");
+//         break;
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuseday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+//         break;
+// }
+
+
+
+// let sum = (a,b) => {
+//     let result = a+b;
+//     return result;
+// }
+
+// console.log(sum(10,20));
+
+
+
+// function createCar (brand, color){
+//     return {
+//         brand: brand,
+//         color: color,
+//         drive: function(){
+//             console.log("started");
+//         }
+//     };
+// }
+
+
+// car1 = createCar("BMW", "red");
+// car2 = createCar("Audi", "blue");
+// car3 = createCar("Mercedes", "black");
+// console.log(car1);
+// console.log(car2);
+// console.log(car3);
+// car1.drive();
+// car2.drive();
+// car3.drive();
+
+
+ 
+
+// function Car (color,brand){
+//     this.color = color;
+//     this.brand = brand;
+//     this.drive = function(){
+//         console.log("started");
+//     };
+// }
+
+
+// let car1 = new Car("red", "BMW");
+// let car2 = new Car("blue", "Audi");
+// console.log(car1);
+// console.log(car2);
+
+
+
+
+
+// function Car (color,brand){
+//     this.color = color;
+//     this.brand = brand;
+//     this.drive = function(){
+//         console.log("started");
+//     };
+// }
+// console.log(Car.name);
+// console.log(Car.length);
+// console.log(typeof(Car));
+
+
+
+// let myObject = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// }
+
+
+// let returnMyObject = myObject;
+// console.log(returnMyObject);
+// console.log(returnMyObject.constructor);
+
+
+
+
+// function Car (color,brand){
+//     this.color = color;
+//     this.brand = brand;
+//     this.drive = function(){
+//         console.log("started");
+//     };
+// }
+
+// let car1 = new Car("red", "BMW");
+// console.log(car1.constructor);
+
+
+// let datecontainerEl = document.getElementById("datecontainer");
+
+// let now = new Date("2022-01-19");
+// console.log(now);
+// console.log(typeof(now));
+
+
+// let date1 = new Date(1947,7,14,11,14,15,0);
+
+// console.log(date1.getFullYear());
+// console.log(date1.getMonth());
+// date1.setFullYear(2023);
+// console.log(date1.getFullYear());
+// console.log(date1.getDate());
+
+  
+
+
+// let car = {
+//     brand: "BMW",
+//     color: "red",
+//     drive: function(){
+//         console.log(this);
+//     }
+// }
+
+
+// car.drive();
+
+
+
+// function add(){
+//     console.log(this);
+// }
+
+// add();
+
+
+
+//  add() => {
+//     console.log(this);
+// }
+
+
+// add();
+
+
+
+
+// let car = {
+//     color :"red",
+//     brand :"BMW",
+//     pavan: function(){
+//         setInterval(() => {
+//             console.log(this);
+//         }, 1000);
+
+//         setTimeout(()=>car.pavan(),1000);
+//     }
+// }
+
+// car.pavan(); 
+
+
+
+// function Car(color,brand){
+//     this.color = color;
+//     this.brand = brand;
+//     this.drive = () => {
+//         console.log("this");
+//     };
+// }
+
+
+// let car1 = new Car("red", "BMW");
+// console.log(car1);
+
+
+
+
+// let x = { value: 20};
+// let y = x;
+// let z = { value: 250};
+// y.value = 30;
+// console.log(x);
+// console.log(y);
+// console.log(z);
+// y = z;
+// console.log(x);
+// console.log(y);
+
+
+
+
+const myObj = {
     name: "John",
-    age: 30,
+    age: 30, 
     city: "New York"
-};
-console.log(`my details are ${JSON.stringify(person)}`); // Output: my details are {"name":"John","age":30,"city":"New York"}
+}
+
+
+let myObj2 = {
+    ...myObj,
+    DOB : "1990-01-01",
+    phone : "1234567890"
+} 
+
+// console.log(myObj2);
+
+myObj = {};
+
+console.log(myObj)
 
